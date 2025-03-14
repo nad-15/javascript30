267465f3bf0b4e56911c0a81ff70cac774802d5e
nad-15 <123393892+nad-15@users.noreply.github.com>
2025-03-15 00:46:27 +0800

    REFAC: Replace setTimeout function to transitionedn event for better performance

--- 01 - JavaScript Drum Kit/script.js
+++ 01 - JavaScript Drum Kit/script.js
@@ -1,20 +1,25 @@
-window.addEventListener('keydown', function(e) {
-    const keyElements = document.querySelectorAll(`[data-key="${e.keyCode}"]`);
+window.addEventListener('keydown', playSound );
 
-    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+function playSound(e) {
+    const keyElement = document.querySelector(`div[data-key="${e.keyCode}"]`); // Select the key div
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`); // Select the corresponding audio
 
-    if (!audio) return; // Stop if no matching audio element
+    if (!audio) return; // Stop if no matching audio
 
-    audio.currentTime = 0; // Rewind to start
+    audio.currentTime = 0; // Reset playback
     audio.play(); // Play the sound
 
-    keyElements.forEach((keyElement) => {
-        console.log(`${e.key} is pressed`);
-        keyElement.classList.add('playing');
+    keyElement?.classList.add('playing'); // Add class if keyElement exists
+    // setTimeout(() => {
+    //     keyElement?.classList.remove('playing'); // Remove class after animation
+    // }, 200);
+}
 
-        setTimeout(() => {
-            keyElement.classList.remove('playing');
-        }, 200);
-    });
+const keys = document.querySelectorAll(`.key`);
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
 
-});
+function removeTransition(e){
+    if(e.propertyName !== `transform`) return;
+    this.classList.remove(`playing`);
+}
+

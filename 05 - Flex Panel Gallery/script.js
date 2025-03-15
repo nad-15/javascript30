326a2db6349a5067ec10a4a2fabb8c5a65ac4a3d
nad-15 <123393892+nad-15@users.noreply.github.com>
2025-03-15 23:40:34 +0800

    REFAC: Implement click event to enlarge panel and mouseleave to reset size

--- 05 - Flex Panel Gallery/script.js
+++ 05 - Flex Panel Gallery/script.js
@@ -5,9 +5,9 @@ allPanel.forEach(panel=> {
     panel.addEventListener(`click`, ()=>{
 
         //remove every open and translate classlist
-        const allParagraph = document.querySelectorAll(`p`);
-        allParagraph.forEach(p => p.classList.remove(`translate`));
-        allPanel.forEach(panels => panels.classList.remove(`open`));
+        // const allParagraph = document.querySelectorAll(`p`);
+        // allParagraph.forEach(p => p.classList.remove(`translate`));
+        // allPanel.forEach(panels => panels.classList.remove(`open`));
 
         //then add to the clicked panel
         const paragraph = panel.querySelectorAll('p:nth-child(odd)');
@@ -18,3 +18,12 @@ allPanel.forEach(panel=> {
     })
 });
 
+allPanel.forEach(panel=> {
+    panel.addEventListener(`mouseleave`, ()=>{
+        const paragraph = panel.querySelectorAll('p:nth-child(odd)');
+        paragraph.forEach(p => {
+            p.classList.remove(`translate`);
+        });
+        panel.classList.remove(`open`);
+    });
+});

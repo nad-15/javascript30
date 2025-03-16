99af5a27081ff20921496b828a13dd15f3ffdef4
nad-15 <123393892+nad-15@users.noreply.github.com>
2025-03-16 20:58:02 +0800

    REFAC: Use toggle classList at click event instead of click+mouseleave combo

--- 05 - Flex Panel Gallery/script.js
+++ 05 - Flex Panel Gallery/script.js
@@ -1,22 +1,22 @@
-// const allPanel = document.querySelectorAll(`.panel`);
+const allPanel = document.querySelectorAll(`.panel`);
 
 
-// allPanel.forEach(panel=> {
-//     panel.addEventListener(`click`, ()=>{
+allPanel.forEach(panel=> {
+    panel.addEventListener(`click`, ()=>{
 
-//         //remove every open and translate classlist
-//         // const allParagraph = document.querySelectorAll(`p`);
-//         // allParagraph.forEach(p => p.classList.remove(`translate`));
-//         // allPanel.forEach(panels => panels.classList.remove(`open`));
+        //remove every open and translate classlist
+        // const allParagraph = document.querySelectorAll(`p`);
+        // allParagraph.forEach(p => p.classList.remove(`translate`));
+        // allPanel.forEach(panels => panels.classList.remove(`open`));
 
-//         //then add to the clicked panel
-//         const paragraph = panel.querySelectorAll('p:nth-child(odd)');
-//         paragraph.forEach(p => {
-//             p.classList.add(`open-active`);
-//         });
-//         panel.classList.add(`open`);
-//     })
-// });
+        //then add to the clicked panel
+        const paragraph = panel.querySelectorAll('p:nth-child(odd)');
+        paragraph.forEach(p => {
+            p.classList.toggle(`open-active`);
+        });
+        panel.classList.toggle(`open`);
+    })
+});
 
 // allPanel.forEach(panel=> {
 //     panel.addEventListener(`mouseleave`, ()=>{
@@ -32,19 +32,19 @@
 
 //Wes Bos Solution
 
-const panels = document.querySelectorAll('.panel');
+// const panels = document.querySelectorAll('.panel');
 
-function toggleOpen() {
-  console.log('Hello');
-  this.classList.toggle('open');
-}
+// function toggleOpen() {
+//   console.log('Hello');
+//   this.classList.toggle('open');
+// }
 
-function toggleActive(e) {
-  console.log(e.propertyName);
-  if (e.propertyName.includes('flex')) {
-    this.classList.toggle('open-active');
-  }
-}
+// function toggleActive(e) {
+//   console.log(e.propertyName);
+//   if (e.propertyName.includes('flex')) {
+//     this.classList.toggle('open-active');
+//   }
+// }
 
-panels.forEach(panel => panel.addEventListener('click', toggleOpen));
-panels.forEach(panel => panel.addEventListener('transitionend', toggleActive));
+// panels.forEach(panel => panel.addEventListener('click', toggleOpen));
+// panels.forEach(panel => panel.addEventListener('transitionend', toggleActive));

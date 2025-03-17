b8efa0b8ed052e7a32de6eb9483f5be69a6a8c5b
nad-15 <123393892+nad-15@users.noreply.github.com>
2025-03-17 21:07:34 +0800

    STYLE: Use e.propertyName to check for the type of transition calling toggleActive.. if its translate then we return

--- 05 - Flex Panel Gallery/script.js
+++ 05 - Flex Panel Gallery/script.js
@@ -1,22 +1,58 @@
 const allPanel = document.querySelectorAll(`.panel`);
 
 
-allPanel.forEach(panel=> {
-    panel.addEventListener(`click`, ()=>{
+// allPanel.forEach(panel=> {
+//     panel.addEventListener(`click`, ()=>{
 
-        //remove every open and translate classlist
-        // const allParagraph = document.querySelectorAll(`p`);
-        // allParagraph.forEach(p => p.classList.remove(`translate`));
-        // allPanel.forEach(panels => panels.classList.remove(`open`));
+//         //remove every open and translate classlist
+//         // const allParagraph = document.querySelectorAll(`p`);
+//         // allParagraph.forEach(p => p.classList.remove(`translate`));
+//         // allPanel.forEach(panels => panels.classList.remove(`open`));
 
-        //then add to the clicked panel
-        const paragraph = panel.querySelectorAll('p:nth-child(odd)');
-        paragraph.forEach(p => {
-            p.classList.toggle(`open-active`);
-        });
-        panel.classList.toggle(`open`);
-    })
-});
+//         //then add to the clicked panel
+//         const paragraph = panel.querySelectorAll('p:nth-child(odd)');
+//         paragraph.forEach(p => {
+//             p.classList.toggle(`open-active`);
+//         });
+
+
+//         panel.classList.toggle(`open`);
+//     })
+// });
+
+allPanel.forEach(panel=> panel.addEventListener(`click`, toggleOpen));
+allPanel.forEach(panel=> panel.addEventListener(`transitionend`, toggleActive));
+
+
+function toggleOpen(e) {
+    this.classList.toggle(`open`);
+}
+
+
+// function toggleActive(e) {
+//         const paragraph =this.querySelectorAll('p:nth-child(odd)');
+//         paragraph.forEach(p => {
+//             p.classList.toggle(`open-active`);
+//         });
+// }
+
+
+function toggleActive(e) {
+    // Ensure that the transitionend event is being triggered by the panel's transition
+    const panel = this;
+    console.log(panel);
+    if (!e.propertyName.includes(`flex`)) return;
+    // if (e.propertyName.includes(`transform`)) return;
+
+
+    // Select all odd <p> elements inside the current panel
+    const paragraphs = panel.querySelectorAll('p:nth-of-type(odd)');
+    paragraphs.forEach(p => {
+        console.log(p);
+        p.classList.toggle('open-active');
+    });
+}
+
 
 // allPanel.forEach(panel=> {
 //     panel.addEventListener(`mouseleave`, ()=>{

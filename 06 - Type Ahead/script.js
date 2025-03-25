f4ff0c122641d9ded0ebfe0e0047c6452fbdb65d
nad-15 <123393892+nad-15@users.noreply.github.com>
2025-03-25 23:48:01 +0800

    FEAT: Finish 06 - Type Ahead

--- 06 - Type Ahead/script.js
+++ 06 - Type Ahead/script.js
@@ -2,25 +2,63 @@ const endpoint = 'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb
 const search = document.querySelector(`.search`);
 const suggestions = document.querySelector(`.suggestions`);
 
-search.addEventListener(`input`, filterCity);
+search.addEventListener(`input`, displayResult);
 
-let cities = [];
+const cities = [];
 
 fetch(endpoint)
     .then(blob => blob.json())
     .then(data => cities.push(...data))
 
+//add comma to numbers
+function numberWithCommas(x) {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+}
 
-function filterCity() {
-    suggestions.innerHTML ="";
-    const searchValue = search.value;
-    if(!searchValue) return;
-    const matchCities = cities.filter(name=> name.city.includes(searchValue) || name.state.includes(searchValue));
-    matchCities.forEach(match => {
-        const city = document.createElement(`li`);
-        city.textContent = match.city;
-        suggestions.appendChild(city);
-    });
+
+function findMatch(wordToMatch, cities) {
+
+    return cities
+    //Originally used .includes with toLowerCase.. wes bos code is better
+        .filter(name => {
+            const regex = new RegExp(wordToMatch, "gi");
+            return name.city.match(regex) || name.state.match(regex);
+        })
+}
+
+
+function displayResult() {
+    const match = findMatch(this.value, cities);
+    console.log(match);
+
+    const html = match.map(place => {
+        const regex = new RegExp(this.value, 'gi');
+        const cityName = place.city.replace(regex, `<span class="hl">${this.value}</span>`);
+        const stateName = place.state.replace(regex, `<span class="hl">${this.value}</span>`);
+        return `
+          <li>
+            <span class="name">${cityName}, ${stateName}</span>
+            <span class="population">${numberWithCommas(place.population)}</span>
+          </li>
+        `;
+      }).join('');
+      suggestions.innerHTML = html;
+
+    // if (!match) return;
+    // match.forEach(match => {
+    //     const list = document.createElement(`li`);
+
+    //     const cityState = document.createElement(`span`);
+    //     const population = document.createElement(`span`);
+    //     population.classList.add('population');
+
+    //     cityState.textContent = `${match.city}, ${match.state}`;
+    //     population.textContent = match.population;
+
+    //     list.appendChild(cityState);
+    //     list.appendChild(population);
+    //     suggestions.appendChild(list);
+    // });
 }
 
 

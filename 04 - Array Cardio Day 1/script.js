3f3396a4eb9598bfc378ada90c391e605fad216c
nad-15 <123393892+nad-15@users.noreply.github.com>
2025-03-25 19:45:00 +0800

    FEAT: Add landing page for all(30) project links

--- 04 - Array Cardio Day 1/script.js
+++ 04 - Array Cardio Day 1/script.js
@@ -61,10 +61,10 @@
 
       // 6. create a list of Boulevards in Paris that contain 'de' anywhere in the name
       // https://en.wikipedia.org/wiki/Category:Boulevards_in_Paris
-      const category = document.querySelector(`.mw-category`);
-      const links =  Array.from(category.querySelectorAll(`a`));
+      // const category = document.querySelector(`.mw-category`);
+      // const links =  Array.from(category.querySelectorAll(`a`));
 
-      const includesDe = links.map(link => link.textContent).filter(boulevard=>boulevard.includes(`de`));
+      // const includesDe = links.map(link => link.textContent).filter(boulevard=>boulevard.includes(`de`));
 
 
 
